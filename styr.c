8e2d2781e5fe8717d082d7242782cd77cc7658fd
Klara <klawi021@student.liu.se>
2013-04-02 15:02:01 +0200

    lade till styr.c

---
+++ styr.c
@@ -0,0 +1,23 @@
+﻿/*
+ * styr.c
+ *
+ * Created: 4/2/2013 2:20:45 PM
+ *  Author: klawi021
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	/* Set */
+	TCCR1A = (1 << COM1A1) | (1 << WGM11);
+	TCCR1B = (1 << WGM11) | (1 << WGM13) | (1 << WGM12) | (1 << CS12) | (1 << CS10);
+	TCCR2A = (1 << COM2A1) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20); 
+	TCCR2B = (1 << CS20);
+	
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
