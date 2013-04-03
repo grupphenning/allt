7ddd760dabd6db43b865e43573dd1fb54576f228
Erik HK <erikhk@gmail.com>
2013-04-03 19:07:23 +0200

    lade till styrmodulsprojektet

---
+++ kod/styrmodul/Styrmodul/Styrmodul/Styrmodul.c
@@ -0,0 +1,137 @@
+﻿/*
+ * Styrmodul.c
+ *
+ * Created: 4/2/2013 2:20:45 PM
+ *  Author: klawi021
+ */ 
+
+
+#include <avr/io.h>
+#include "bitmacros.h"
+
+
+#define LEFT_DIR PB0
+#define RIGHT_DIR PB1
+#define PORT_DIR PORTB
+
+#define LEFT_AMOUNT OCR2A
+#define RIGHT_AMOUNT OCR2B
+
+#define LEFT_PWM PD7
+#define RIGHT_PWM PD6
+#define PORT_PWM PORTD
+
+
+int main(void)
+{
+	//pwm-styrning för gripklon, pin OC1A, register OCR1A
+	TCCR1A = 0;
+	setbit(TCCR1A, COM1A1);
+	setbit(TCCR1A, WGM11);
+	
+	TCCR1B = 0;
+	setbit(TCCR1B, WGM11);
+	setbit(TCCR1B, WGM12);
+	setbit(TCCR1B, WGM13);
+	
+	//sätt klockan, fc = f/1024
+	setbit(TCCR1B, CS10);
+	setbit(TCCR1B, CS12);
+	
+	//TCCR1A = (1 << COM1A1) | (1 << WGM11);
+	//TCCR1B = (1 << WGM11) | (1 << WGM13) | (1 << WGM12) | (1 << CS12) | (1 << CS10);
+	//TIMSK1 = (1 << OCIE1A);  // Enable Interrupt TimerCounter1 Compare Match A (TIMER1_COMPA_vect)
+	//sätt OCR1A också!
+	
+	//pwm-styrning för motorerna, pinne OC2A, register OCR2A för vänster, pinne OC2B, register OCR2B för höger.
+	//PB1 DIR höger, PB0 vänster
+	TCCR2A=0;
+	setbit(TCCR2A, COM2A1);
+	setbit(TCCR2A, COM2B1);
+	setbit(TCCR2A, WGM20);
+	setbit(TCCR2A, WGM21);
+	OCR2A = 0;
+	OCR2B = 0;
+	
+	//TCCR2A = (1 << COM2A1) | (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
+	
+	//sätter på pwm!
+	TCCR2B=0;
+	setbit(TCCR2B, CS20);
+	//TCCR2B = (1 << CS20);
+	//TIMSK2 = (1 << OCIE2A);
+	//fullt ös på OCR=0xff, inget på 0x00
+	
+	
+    while(1)
+    {
+		
+    }
+}
+
+
+void drive_forwards(uint8_t amount)
+{
+	LEFT_AMOUNT = amount;
+	RIGHT_AMOUNT = amount;
+	
+	//sätt ettor (framåt) på DIR-pinnarna
+	setbit(PORT_DIR, LEFT_DIR);
+	setbit(PORT_DIR, RIGHT_DIR);
+}
+
+void drive_backwards(uint8_t amount)
+{
+	LEFT_AMOUNT = amount;
+	RIGHT_AMOUNT = amount;
+	
+	//sätt nollor (bakåt) på DIR-pinnarna
+	clearbit(PORT_DIR, LEFT_DIR);
+	clearbit(PORT_DIR, RIGHT_DIR);
+}
+
+//sväng vänster!
+void turn_left(uint8_t amount)
+{
+	LEFT_AMOUNT = amount;
+}
+
+//sväng höger!
+void turn_right(uint8_t amount)
+{
+	RIGHT_AMOUNT = amount;
+}
+
+//stanna allt!
+void stop_motors()
+{
+	//sätter ingen klocka
+	clearbit(TCCR2B, CS20);
+	clearbit(TCCR2B, CS21);
+	clearbit(TCCR2B, CS22);
+	LEFT_PWM = 0;
+	RIGHT_PWM = 0;
+	LEFT_AMOUNT = 0;
+	RIGHT_AMOUNT = 0;
+}
+
+//sväng vänster som en stridsvagn!
+void tank_turn_left(uint8_t amount)
+{
+	LEFT_AMOUNT = amount;
+	RIGHT_AMOUNT = amount;
+	
+	clearbit(PORT_DIR, LEFT_DIR);
+	setbit(PORT_DIR, RIGHT_DIR);
+}
+
+
+//sväng höger som en stridsvagn!
+void tank_turn_right(uint8_t amount)
+{
+	LEFT_AMOUNT = amount;
+	RIGHT_AMOUNT = amount;
+	
+	setbit(PORT_DIR, LEFT_DIR);
+	clearbit(PORT_DIR, RIGHT_DIR);
+}
