08ac8a67dab5342154bf6dc1b560532587352395
Simli <simli427@student.liu.se>
2013-04-09 11:05:22 +0200

    lade till det tomma projektet sensormodul //HK

---
+++ kod/sensormodul/sensormodul/sensormodul.c
@@ -0,0 +1,17 @@
+﻿/*
+ * sensormodul.c
+ *
+ * Created: 2013-04-09 11:03:56
+ *  Author: simli427
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
