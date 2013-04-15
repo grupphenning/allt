183beddf3bc031f5f4aa280d0aab61df3e8e0708
Simli <simli427@student.liu.se>
2013-04-15 17:56:49 +0200

    Han koda lite grann

--- kod/styrmodul/Styrmodul/Styrmodul/pid.c
+++ kod/styrmodul/Styrmodul/Styrmodul/pid.c
@@ -2,3 +2,13 @@
  * pid.c
  */ 
 #include "display.h"
+
+void update_k_values(uint8_t k_prop, uint8_t k_int, uint8_t k_der)
+{
+	
+}
+
+void regulator()
+{
+	
+}
--- kod/styrmodul/Styrmodul/Styrmodul/pid.h
+++ kod/styrmodul/Styrmodul/Styrmodul/pid.h
@@ -5,7 +5,14 @@
 #define PID_H
 #define F_CPU 8000000UL // 8 MHz Internal oscillator
 
+uint8_t k_prop, k_int, k_der;
+int8_t error_sum, last_d_value;
+uint16_t cykle_time; //Vilken frekvens reglering körs.
+uint8_t max_out; //Maxvärde för utsignalen, undvika fel med mättad styrsignal.
 
 
+void update_k_values(uint8_t k_prop, uint8_t k_int, uint8_t k_der);
+
+void regulator();
 
 #endif // PID_H
