84a381d9020048cd0ccd4a74afd54c729f6c4306
tfk-32 <grupphenning@gmail.com>
2013-04-25 11:13:44 +0200

    Reglerfix

--- kod/styrmodul/Styrmodul/Styrmodul/Styrmodul.c
+++ kod/styrmodul/Styrmodul/Styrmodul/Styrmodul.c
@@ -25,7 +25,7 @@ volatile uint8_t spi_data_from_sensor[BUF_SZ];
 uint8_t spi_sensor_read;
 volatile uint16_t spi_sensor_write;
 
-#define SPEED 255
+#define SPEED 100
 uint8_t ninety_timer, turn, pid_timer;
 uint8_t left = 1;
 
@@ -87,8 +87,8 @@ int main(void)
 	sei();		//aktivera global interrupts
 	
 	clear_pid();
-	init_pid(40, 70, -70);
-	update_k_values(1, 1, 1);
+	init_pid(40, 100, -100);
+	update_k_values(10, 1, 4);
 	
 	while(1)
 	{
