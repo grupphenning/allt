bb5f13a504733a6b8fe5d87489307178f1e49ba7
rasme879 <rasme879@student.liu.se>
2013-05-17 10:51:03 +0200

    fardigkommenterad sensormodul.c

--- kod/sensormodul/sensormodul/sensormodul.c
+++ kod/sensormodul/sensormodul/sensormodul.c
@@ -1,9 +1,9 @@
 ﻿/*
- * sensormodul.c
- *
- * Created: 2013-04-09 11:03:56
- *  Author: eriha181
- */
+* sensormodul.c
+*
+* Created: 2013-04-09 11:03:56
+*  Author: eriha181
+*/
 //definiera klockhastighet
 #define F_CPU 8000000UL
 #include "sensormodul.h"
@@ -19,24 +19,41 @@ uint8_t tape_sensor;
 uint8_t read_mode; // Om read_mode == 1, integrera gyro, annars läs IR och tejp.
 char tape_type;
 
-int16_t gyro_init_value;						//Gyrots initialvärde
+//Gyrots initialvärde
+int16_t gyro_init_value;
 int16_t full_turn, gyro_int;
 
+//Array med data från alla nio reflexsensorer.
 volatile uint8_t tape_sensor_data[9];
+//Array med data från alla fem IR-sensorer, med en extra plats för
+// en flagga som säger vilken typ av data det är.
 volatile uint8_t ir_sensor_data[5+1];
+//Array med data som säger vilken typ av tejp som hittats,
+//med en extra plats för en flagga som säger vilken typ av data det är.
 volatile uint8_t decoded_tape_data[1+1];
+//Array med data som säger vilken position roboten befinner sig på i
+//linjeföljningen, med en extra plats för en flagga som säger
+//vilken typ av data det är.
 volatile uint8_t tape_position[1+1];
 volatile uint8_t test_data[16+1];
+//Anger vilket index datan börjar på i ir_sensor_data och tape_sensor_data.
 volatile uint8_t data_index=1;
+//Flagga som anger om adc:n skickat interrupt.
 volatile uint8_t adc_interrupt;
+//Anger om data finns att hämta på bussen.
 volatile uint8_t has_data_from_spi;
 volatile uint8_t read_and_send_ir_to_master;
+//Flagga som anger om linjeföljningsmodet är aktiverat.
 volatile uint8_t follow_end_tape;
+//Flagga som anger om det autonoma modet är aktiverat.
 volatile uint8_t autonomous;
 volatile uint8_t to_read_gyro;
-volatile uint8_t tape_interupt_flag;
+volatile uint8_t tape_interrupt_flag;
 volatile uint8_t spi_transfer_complete;
 
+/*
+Sparar ner data från bussen.
+*/
 void read_byte(uint8_t b)
 {
 	has_data_from_spi = 1;
@@ -58,15 +75,12 @@ int main(void)
 	init_spi();
 	init_adc();
 	init_gyro();
-	//init_gyro_timer();
 	init_sensor_timer();
 	
 	setbit(DDRB, PORTB3);
 
-	
-	
-    while(1)
-    {
+	while(1)
+	{
 		if(read_mode) {
 			if(read_and_send_ir_to_master)
 			{
@@ -76,14 +90,14 @@ int main(void)
 				values[0] = SENSOR_GYRO_INTEGRAL;
 				values[1] = (gyro_int) >> 8;
 				values[2] = (gyro_int);
-				send_to_master(3, values);					
+				send_to_master(3, values);
 			}
 			if(to_read_gyro)
 			{
 				to_read_gyro = 0;
-			}				
+			}
 		}
-		else {			
+		else {
 			if (read_and_send_ir_to_master && !follow_end_tape)
 			{
 				read_and_send_ir_to_master = 0;
@@ -98,7 +112,7 @@ int main(void)
 				}
 				regulate_end_tape();
 			}
-		}			
+		}
 
 		
 		if(SPSR & (1 << SPIF)) read_byte(SPDR);
@@ -127,21 +141,9 @@ int main(void)
 				read_mode = 0;
 				gyro_int = 0;
 			}
-			
-// 			switch(spi_data_to_master)
-// 			{
-// 				case TURN_RIGHT:
-// 					begin_turning(90);
-// 					break;
-// 				case TURN_LEFT:
-// 					begin_turning(-90);
-// 					break;
-// 				default:
-// 					break; 
-// 			}
 
 			has_data_from_spi = 0;
-		}	
+		}
 		
 		if (!follow_end_tape)
 		{
@@ -149,43 +151,18 @@ int main(void)
 			if(tape_sensor)
 			{
 				decode_tape();
-				//send_decoded_tape(); //Onödigt skicka när beslut ej tagits.
 			}
-			tape_interupt_flag = 0;			
-		}		
-    }
+			tape_interrupt_flag = 0;
+		}
+	}
 }
-
-/* VAFAN ÄR DETTA??
-<<<<<<< HEAD
-void init_gyro_timer()
-{
-	
-}
-
-
-void read_all_sensors()
-=======
-*/
 
 /* =======================================GYRO=======================================*/
 
-
-
-// Börja snurra. positiv degrees = medurs. Har slutat då is_turning blir 0.
-void begin_turning(int16_t degrees)
-//>>>>>>> f840052e7a203feb98207f4cf649ee4656753730 //wtf??
-{
-	full_turn = 55*abs(degrees);	//Sväng x antal grader
-	gyro_int = 0;
-//	init_gyro_timer();
-
-}
-
 /*
- * Initerar gyrot genom att utföra ett fåtal avläsningar,
- * kastar dessa som skräpvärden och sedan väljer ett som initialvärde till gyrot
- */
+* Initierar gyrot genom att utföra ett fåtal avläsningar,
+* kastar dessa som skräpvärden och sedan väljer ett som initialvärde till gyrot
+*/
 void init_gyro()
 {
 	static uint8_t gyro_reads_number = 0;
@@ -195,34 +172,18 @@ void init_gyro()
 		read_gyro();
 		gyro_reads_number++;
 	}
-	//uint16_t sum = 0;
 	uint8_t i;
-	//for(i = 0; i < 16; ++i) sum += read_gyro();
 	gyro_init_value = 0;
 	for(i = 0; i < 1; ++i) gyro_init_value += read_gyro();
 	gyro_init_value += 1;
-	//gyro_init_value = sum;
-}
-
-
-// 
-void timed_gyro_turn()
-{
-	gyro_int += 3 * abs(gyro_init_value - (uint8_t) read_gyro());			//Maxhastighet 300grader/s,
-	if(gyro_int >= full_turn)										//maxvärde-nollnivå ung 100.
-	{
-		disable_gyro_timer();
-		send_stop_turn_message();
-	}
 }
 
 
 /* ====================================SENSORAVLÄSNING=======================================*/
 
 /*
- * Initiera adc-omvandling
- */
-
+* Initiera adc-omvandling
+*/
 void init_adc()
 {
 	//reference voltage
@@ -233,13 +194,6 @@ void init_adc()
 	
 	//Left adjust measurment
 	setbit(ADMUX, ADLAR);
-	//	setbit(ADMUX, MUX3);		// 10x gain!
-	
-	//Set ADC in free running- mode
-	//setbit(ADCSRA,ADATE);
-	
-	//Enable interupt
-	//setbit(ADCSRA,ADIE);
 	
 	//SKalning av klockfrekvens
 	setbit(ADCSRA,ADPS2);
@@ -248,41 +202,8 @@ void init_adc()
 }
 
 /*
- * Läs av alla sensorer ( Ej gyro)
- */
-void read_all_sensors()
-{
-	setbit(PORTC, PINC0);		// For debug!
-	data_index = 1;
-	test_data[0] = SENSOR;
-	
-	read_ir(0);
-	read_ir(1);
-	read_ir(2);
-	read_ir(3);
-	read_ir(4);
-	
-	read_tape(0);
-	read_tape(1);
-	read_tape(2);
-	read_tape(3);
-	read_tape(4);
-	read_tape(5);
-	read_tape(6);
-	read_tape(7);
-	read_tape(8);
-	read_tape(9);
-	read_tape(10);
-	clearbit(PORTC, PINC0);		// For debug!!
-	send_to_master(16, test_data);
-	setbit(PORTC, PINC0);		// For debug!!
-	clearbit(PORTC, PINC0);		// For debug!!
-}
-
-/*
 * Läser och skickar data från ir-sensorerna
 */
-
 void read_and_send_ir()
 {
 	data_index = 1;
@@ -294,6 +215,10 @@ void read_and_send_ir()
 	send_to_master(6, ir_sensor_data);
 }
 
+
+/*
+Skickar (till styrmodulen) vilken typ av tejp som hittats.
+*/
 void send_decoded_tape()
 {
 	data_index = 1; //Borde inte behövas, dubbelkolla det!
@@ -302,31 +227,10 @@ void send_decoded_tape()
 	send_to_master(2, decoded_tape_data);
 }
 
-
-/* 
- * Läser bara av tejpsensorer, skicka rådata eller beräkna mittpunkt här?!
- */
-
-
-void read_and_send_tape()
-{
-	data_index = 1;
-	test_data[0] = SENSOR;
-	uint8_t i;
-	for(i = 0; i < 11; i++)
-	{
-		read_tape(i);
-	}
-	
-	send_to_master(11, test_data);
-}
-
 /*
- * Läs av gyrot.
- * OBS!! Använder inte read_adc() likt andra sensoravläsningsfunktioner
- */
-
-
+* Läs av gyrot.
+* OBS!! Använder inte read_adc() likt andra sensoravläsningsfunktioner
+*/
 uint8_t read_gyro()
 {
 	clearbit(ADMUX,MUX0);
@@ -334,16 +238,13 @@ uint8_t read_gyro()
 	clearbit(ADMUX,MUX2);
 	clearbit(ADMUX,MUX3);
 	clearbit(ADMUX,MUX4);
-	//read_adc();
 	setbit(ADCSRA,ADSC); //start_reading
-	//while(!adc_interrupt); //Wait for interupt to occur
 	while(bitclear(ADCSRA, ADIF));
 	return ADCH;
 }
 
-
 /*
-* Läs av ir-sensor med nr sensor_no
+* Läs av ir-sensor med nr sensor_no.
 */
 void read_ir(uint8_t sensor_no)
 {
@@ -367,8 +268,8 @@ void read_ir(uint8_t sensor_no)
 
 
 /*
- * Läs av tape-sensor med nr sensor_no
- */
+* Läs av tape-sensor med nr sensor_no.
+*/
 void read_tape(uint8_t sensor_no)
 {
 	clearbit(ADMUX,MUX0);
@@ -382,6 +283,11 @@ void read_tape(uint8_t sensor_no)
 	read_adc_tape();
 }
 
+/*
+Läs enbart av en reflexsensor,
+används för alla tejpbitar utom linjeföljningstejpen,
+då nio av reflexsensorerna används.
+*/
 void read_one_tape()
 {
 	clearbit(ADMUX,MUX0);
@@ -401,12 +307,13 @@ void read_one_tape()
 void read_adc_tape()
 {
 	setbit(ADCSRA,ADSC); //start_reading
-	//while(!adc_interrupt); //Wait for interrupt to occur
 	while(bitclear(ADCSRA, ADIF));
 	tape_sensor_data[data_index++] = ADCH;
-	//adc_interrupt = 0;
 }
 
+/*
+Läs AD-omvandlaren för enbart en tejpsensor.
+*/
 void read_adc_one_tape()
 {
 	setbit(ADCSRA,ADSC); //start_reading
@@ -414,13 +321,14 @@ void read_adc_one_tape()
 	tape_sensor = ADCH;
 }
 
+/*
+Läser AD-omvandlaren för IR-sensorerna.
+*/
 void read_adc_ir()
 {
 	setbit(ADCSRA,ADSC); //start_reading
-	//while(!adc_interrupt); //Wait for interupt to occur
 	while(bitclear(ADCSRA, ADIF));
 	ir_sensor_data[data_index++] = ADCH;
-	//adc_interrupt = 0;
 }
 
 
@@ -437,7 +345,6 @@ void decode_tape()
 	
 	if (tape_sensor > REFLEX_SENSITIVITY) //Tejpbitsstart hittad
 	{
-		//cli(); //Stänger av globala avbrott
 		is_over_tape = 1;
 		no_tape_count = 0;
 		tape_count++;
@@ -450,23 +357,14 @@ void decode_tape()
 		if(is_in_tape_segment) //Andra tejpbiten avslutad
 		{
 			is_in_tape_segment = 0;
-			//second_tape = tape_count < 5 ? 's': 'l';
 			second_tape_count = tape_count;
 			uint8_t tmp[5];
-// 			tmp[0] = SENSOR_TAPE_DEBUG;
-// 			tmp[1] = tape_count;
-// 			send_to_master(2,tmp);
 			decode_tape_segment(first_tape_count, second_tape_count); //Kör tejpsegmentsavkodning
 		}
 		else if(tape_count != 1) // Första tejpbit avslutad		OBS: Fulhack som löste en sensorbugg.
 		{
 			is_in_tape_segment = 1;
-			//first_tape = tape_count < 5 ? 's': 'l';
 			first_tape_count = tape_count;
-// 			uint8_t tmp[5];
-// 			tmp[0] = SENSOR_TAPE_DEBUG;
-// 			tmp[1] = tape_count;		
-//			send_to_master(2,tmp);
 		}
 		tape_count=0;
 	}
@@ -479,10 +377,7 @@ void decode_tape()
 		{
 			is_in_tape_segment = 0;
 			follow_end_tape = 1;
-			//decode_tape_segment(first_tape_count, 0); // Skickas vid början av linjeföljningen!
-			
 		}
-		//is_in_tape_segment = no_tape_count++ > 7 ? 0 : is_in_tape_segment; //vilken jävla oneliner!
 		
 		
 		//overflowskydd
@@ -501,9 +396,7 @@ void decode_tape_segment(uint16_t first, uint8_t second)
 	{
 		tape_type = 'g';
 		send_decoded_tape();
-		return;	
-// 		first = first*10;
-// 		tape_ratio = first/second;
+		return;
 	}
 	
 	if(first <= 100 && second > 100) //(first == 's' && second == 'l')
@@ -526,6 +419,10 @@ void decode_tape_segment(uint16_t first, uint8_t second)
 	}
 }
 
+/*
+Räknar ut positionen som roboten befinner sig på i linjeföljningen,
+finns utförligt beskrivet i tekniska dokumentationen.
+*/
 void regulate_end_tape()
 {
 	int8_t pos_index; //-5 för längst till vänster, 5 för höger, 0 i mitten!
@@ -571,8 +468,8 @@ void regulate_end_tape()
 /*=======================================TIMERS==================================*/
 
 /*
- * Initiera timer kopplad till sensoravälsning
- */
+* Initiera timer kopplad till sensoravläsning
+*/
 void init_sensor_timer()
 {
 	setbit(TCCR1A, WGM11);
@@ -593,12 +490,10 @@ void init_sensor_timer()
 	
 }
 
-//Sensor timer! (25 Hz)
-// ISR(TIMER1_OVF_vect)
-// {
-// 	read_all_sensors();
-// }
-
+/*
+Avbrottsrutin för timern som skickar IR-data till styrenheten,
+25-50 Hz ungefär.
+*/
 ISR(TIMER1_OVF_vect)
 {
 	read_and_send_ir_to_master = 1;
@@ -606,27 +501,25 @@ ISR(TIMER1_OVF_vect)
 	//tape_interupt_flag = 1;
 }
 
-//Gyro timer! ( Hz)
+//Gyro timer! (~30 Hz)
 ISR(TIMER2_OVF_vect)
-{        
+{
 	to_read_gyro = 1;
 }
 
 /*
- * Avaktivera timern kopplad till gyrot
- */
+* Avaktivera timern kopplad till gyrot
+*/
 void disable_gyro_timer()
 {
 	clearbit(TIMSK, TOIE2);
 }
 
 /*
- * Initeietra timer kopplad till gyrot (TIMER2)
- */
+* Initeietra timer kopplad till gyrot (TIMER2)
+*/
 void init_gyro_timer()
 {
-	// WTF? Waveform?
-//	setbit(TCCR2, WGM20);
 	//sätt CTC mode med OCR2 som topp!
 	setbit(TCCR2, WGM21);
 	
@@ -639,7 +532,6 @@ void init_gyro_timer()
 	setbit(TCCR2, CS21);
 	setbit(TCCR2, CS22);
 	
-
 	//aktivera interrupt på overflow
 	setbit(TIMSK, TOIE2);
 
@@ -647,12 +539,11 @@ void init_gyro_timer()
 
 /*=====================================SPI========================================*/
 /*
- *Initiera spi-bussen
- */
+*Initiera spi-bussen
+*/
 void init_spi()
 {
 	setbit(SPCR, SPE);		//Enables spi
-//	setbit(SPCR, SPIE);
 	clearbit(DDRB, PINB4);	// SS är input
 	clearbit(DDRB, PINB5);	// MOSI är input
 	setbit(DDRB, PINB6);	// MISO är output
@@ -662,20 +553,17 @@ void init_spi()
 	sei();
 }
 
+/*
+Avbrottsrutin för SPI-bussen.
+*/
 ISR(SPI_STC_vect)
 {
-// 	uint8_t byte;
-// 	byte = SPDR;
-// 	if(byte) {
-// 		has_data_from_spi = 1;
-// 		spi_data_to_master = byte;
-// 	}
 	spi_transfer_complete = 1;
 }
 
 /*
- * Skickar en sträng till kontrollern som dumpar denna på displayen.
- */
+* Skickar en sträng till kontrollern som dumpar denna på displayen.
+*/
 void send_string_to_master(char *str)
 {
 	uint8_t len = strlen(str) + 1;	/* Längden av strängen + kommandot */
@@ -687,9 +575,9 @@ void send_string_to_master(char *str)
 }
 
 /*
- *Skicka en byte till master
- *OBS!! om du vill sända en byte använd inte denna! Använd send_to_master(1,byte)
- */
+*Skicka en byte till master
+*OBS!! om du vill sända en byte använd inte denna! Använd send_to_master(1,byte)
+*/
 void send_to_master_real(uint8_t byte)
 {
 	volatile uint8_t a;
@@ -702,29 +590,21 @@ void send_to_master_real(uint8_t byte)
 	a = SPDR;
 }
 
-void send_stop_turn_message()
-{
-	send_to_master(1,GYRO);	//Kanske behöver lägga GYRO i en array ?
-}
-
-
-
-
 /*
- * Denna funktion tar argument enligt följande:
- * len		antalet byte att skicka
- * *data	en pekare till en uint8_t-array av den data som ska skickas.
- *			Första byten är ett kommando, resten är argument.
- *			Kommandon finns definierade i sensormodul.h
- *			Se exempel i main()
- *
- * Protokollet som faktiskt används för att skicka data till styrenheten 
- * över bussen är enligt följande:
- * Byte:
- *	1				längd av datapaket (max 255)
- *  2				Kommando
- *  3 -	(len-2)		eventuella argument
- */
+* Denna funktion tar argument enligt följande:
+* len		antalet byte att skicka
+* *data	en pekare till en uint8_t-array av den data som ska skickas.
+*			Första byten är ett kommando, resten är argument.
+*			Kommandon finns definierade i sensormodul.h
+*			Se exempel i main()
+*
+* Protokollet som faktiskt används för att skicka data till styrenheten
+* över bussen är enligt följande:
+* Byte:
+*	1				längd av datapaket (max 255)
+*  2				Kommando
+*  3 -	(len-2)		eventuella argument
+*/
 void send_to_master(uint8_t len, uint8_t *data)
 {
 	
@@ -733,13 +613,6 @@ void send_to_master(uint8_t len, uint8_t *data)
 	uint8_t sum = 0;
 	while(i < len)
 	{
-		/* FIXME!!!
-		   Jag gillar verkligen inte den här! Kan man inte kolla
-		   att en byte har skickats innan man skickar nästa istället
-		   för att ha en ful delay? Eller vad är det som gör att det inte
-		   funkar utan delay?
-		 */
-		//_delay_ms(1);
 		sum += data[i];
 		send_to_master_real(data[i++]);
 	}
